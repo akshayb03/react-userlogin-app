151ad9308b44a995f5988ecf2fd40dcdb631e254
Akshay <akshaybhati003@gmail.com>
2022-04-17 09:27:48 +0530

    create new project

---
+++ src/components/User.js
@@ -0,0 +1,15 @@
+
+const User = () => {
+    return (
+        <form>
+            <div className="form-control">
+                <div className="form-control-input">
+
+                </div>
+                <div className="form-control-input">
+
+                </div>
+            </div>
+        </form>
+    )
+}
