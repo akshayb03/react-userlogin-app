88fb14a6dbcd91385ac64eb37c1b1cad52576c06
Akshay <akshaybhati003@gmail.com>
2022-04-18 15:02:55 +0530

    css changes

--- src/components/User.js
+++ src/components/User.js
@@ -20,7 +20,7 @@ const User = (props) => {
         }
 
         if (!Data.userAge || Data.userName.length===0) {
-            console.log('Inside');
+            // console.log('Inside');
             setErrorContent(<ErrorHandler name={Data.userName} age={Data.userAge}></ErrorHandler>);
             setEnteredName('');
             setEnteredAge('');
@@ -64,12 +64,12 @@ const User = (props) => {
             <form onSubmit={submitHandler}>
                 <div className="form-control">
                     <div className="form-control-input">
-                        <label>Name</label>
-                        <input style={{backgroundColor:!isValidName?'red':'transparent'}} type="text" value={enteredName} onChange={nameChangeHandler}/><br></br>
+                        <label style={{color:!isValidName?'red':'black'}}>Name</label>
+                        <input style={{backgroundColor:!isValidName?'salmon':'transparent'}} type="text" value={enteredName} onChange={nameChangeHandler}/><br></br>
                     </div>
                     <div className="form-control-input">
-                        <label>Age</label>
-                        <input style={{backgroundColor:!isValidAge?'red':'transparent'}} type="number" value={enteredAge} onChange={ageChangeHandler}/><br></br><br></br>
+                        <label style={{color:!isValidAge?'red':'black'}}>Age</label>
+                        <input style={{backgroundColor:!isValidAge?'salmon':'transparent'}} type="number" value={enteredAge} onChange={ageChangeHandler}/><br></br><br></br>
                     </div>
                     <div className="form-control-button">
                         <button type="submit">Add</button>
